5d611827213f9c906613738fffba64ee563f9f71
davidpodhola <david.podhola@naseukoly.cz>
2014-02-19 17:20:01 +0100

    Database access added

--- FsCsMvc41/FsCsMvc41WebAppSpa/Controllers/ContactsController.fs
+++ FsCsMvc41/FsCsMvc41WebAppSpa/Controllers/ContactsController.fs
@@ -6,19 +6,39 @@ open System.Web.Mvc
 open System.Net.Http
 open System.Web.Http
 open FsWeb.Models
+open System.Data
+open System.Data.Linq
+open Microsoft.FSharp.Data.TypeProviders
+open Microsoft.FSharp.Linq
+
+// USE [Flores_Demo]
+// CREATE TABLE [dbo].[Persons](
+// 	[ID] [char](10),
+// 	[LastName] [varchar](30) ,
+// 	[FirstName] [varchar](20)
+// )
+
+type dbSchema = SqlDataConnection<"Data Source=.;Initial Catalog=Flores_Demo;Integrated Security=SSPI;">
 
 type ContactsController() =
     inherit ApiController()
 
-    // This is for demonstration purposes only. 
-    let contacts = seq { yield Contact(FirstName = "John", LastName = "Doe", Phone = "123-123-1233")
-                         yield Contact(FirstName = "Jane", LastName = "Doe", Phone = "123-111-9876") }
+    let db = dbSchema.GetDataContext()
+
+    let persons =
+        query {
+            for row in db.Persons do
+            select row
+        }
 
     // GET /api/contacts
+    // http://msdn.microsoft.com/en-us/library/hh361033.aspx & http://msdn.microsoft.com/en-us/library/dd233209.aspx
     member x.Get() = 
-        // TODO: Replace with your code to retrieve the contacts list
-        contacts
+        seq { for row in persons -> new Contact(FirstName = row.FirstName, LastName = row.LastName, Phone = "123 456 789" ) }
+
     // POST /api/contacts
     member x.Post ([<FromBody>] contact:Contact) = 
-        // TODO: Replace with your code to persiste the contact information
-        contacts |> Seq.append [ contact ] 
+        let newPerson = new dbSchema.ServiceTypes.Persons( FirstName=contact.FirstName, LastName=contact.LastName )
+        db.Persons.InsertOnSubmit(newPerson)
+        db.DataContext.SubmitChanges()
+        x.Get()
